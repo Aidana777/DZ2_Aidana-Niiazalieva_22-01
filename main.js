4c73e944a2ef347d11dbedaa215baa936553fa7e
Aidana777 <aidananiazalieva27@gmail.com>
2023-02-16 22:26:00 +0600

    curcle

--- main.js
+++ main.js
@@ -1,36 +1,25 @@
 
 
-// const counter = 0;
+let counter = 0;
 
-// timer = setInterval(function () {
-//    counter++
-//    if (counter >= 0) {
-//       id = document.getElementById('seconds')
-//       id.innerHTML = counter
-//    }
-//    if (counter === 60) {
-//       clearInterval(timer)
-//       alert('Done!!!')
-//    }
-// }, 50)
+timer = setInterval(function () {
+   counter++
+   if (counter >= 0) {
+      id = document.getElementById('seconds')
+      id.innerHTML = counter
+   }
+   if (counter === 60) {
+      clearInterval(timer)
+      alert('Done!!!')
+   }
+}, 50)
 
 const box = document.querySelector('.box');
 const block = document.querySelector('.block');
-let positionX = 0;
-let positionY = 0;
-const move = () => {
-    if (positionX <= 470) {
-        positionX += 16;
-        box.style.left = `${positionX}px`;
-        setTimeout(move, 100)
-    } else if (positionX >= 470 && positionY <= 470) {
-        positionY += 16;
-        box.style.top = `${positionX}px`;
-        setTimeout(move, 100)
-    } else if (positionY <= 470 && positionX >= 470) {
-        positionX += 16;
-        box.style.right = `${positionX}px`;
-        setTimeout(move, 100)
-    }
-}
-move()
+let position = 0;
+
+setInterval(function () {
+    position += 3.14 / 70
+    box.style.top = `${235 + 250 * Math.cos(position)}px`
+    box.style.right = `${235 + 250 * Math.sin(position)}px`
+}, 50)
